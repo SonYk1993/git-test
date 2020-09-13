3e2d63581c80a297803ec3323fa71ab26956a62e
SonYk1993 <anti-magedemon@mail.ru>
2020-09-13 17:10:35 +0300

    ferst commit

---
+++ scrypt.js
@@ -0,0 +1,13 @@
+// if (document.getElementById('btn1').onclick){
+//     alert("close me please");
+// }
+
+
+document.getElementById('pilot').addEventListener("click", function(){
+        alert('Hello')
+})
+
+// test100.onclick=function () {
+//     alert("govno");
+    
+// }
