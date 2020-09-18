d8d6253b5c9a0bb641cfaceb0a269f456b53c6cc
SonYk1993 <anti-magedemon@mail.ru>
2020-09-19 01:25:48 +0300

    new proj

---
+++ 1/script.js
@@ -0,0 +1,10 @@
+function fun1() {
+    var chbox;
+    chbox=document.getElementById('one');
+        if (chbox.checked) {
+           document.getElementsById("li-ferst").getElementById="li-check";
+        }
+        else {
+            alert ('Не выбран');
+        }
+    }
